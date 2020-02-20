ac045cf798a08e40239a57b56887f463dbd34f8a
juissi999 <jussi.kaatiala@gmail.com>
2020-02-20 12:38:42 +0200

    Device choosing input reading added

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 use pcap::Device;
+use std::io;
 
 fn main() {
     // list all devices
     let devices = Device::list().unwrap();
+
+    let mut devindice = 0;
+
     for device in devices {
-        println!("Found device {:?}", device.name);
+        println!("Device {} {:?}", devindice, device.name);
+        devindice+=1;
     }
-    
+
+    println!("Choose device (num) >");
+    let mut selection = String::new();
+    io::stdin().read_line(&mut selection)
+        .expect("Failed to read line.");
+
+    let selection: u32 = selection.trim().parse()
+        .expect("Please type a number.");
+
+    if 0 < selection && selection < devindice {
+        println!("Choosing device {}", selection);
+    } 
 }
