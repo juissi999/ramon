ff54c3f73367980db40daa0da858b4afa44f63f7
juissi999 <jussi.kaatiala@gmail.com>
2020-02-24 00:28:18 +0200

    Added comments and improved packet printing

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let devices = Device::list().unwrap();
 
     let mut devindice = 0;
-
     for device in &devices {
         println!("Device {} {:?}", devindice, device.name);
         devindice+=1;
     }
 
+    // read users device choise
     println!("Choose device (num) >");
     let mut selection = String::new();
     io::stdin().read_line(&mut selection)
@@ -20,17 +20,22 @@ fn main() {
     let selection: u32 = selection.trim().parse()
         .expect("Please type an integer.");
 
+    // get the device with that indice
     if selection < devindice {
-        // let mut cap = &devices[selection as usize]; //.open().unwrap();
         let dn = String::from(&devices[selection as usize].name);
         let dn1: &str = &dn;
-        println!("Choosing device {}", dn1);
 
+        println!("Choosing device {}", dn1);
         let mut cap = pcap::Capture::from_device(dn1).unwrap().open().unwrap(); //.open().unwrap();
 
+        // print packets continously
+        let mut index: u32 = 1;
         loop {
             // get a packet and print its bytes
-            println!("{:?}", cap.next());
+            let p = cap.next().unwrap();
+            println!("packet {}:", index);
+            println!("{:?}\n{:?}", p.header, p.data);
+            index += 1;
         }
     }
 }
