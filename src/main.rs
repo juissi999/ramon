02711e31baa870644fef0a1df13e1623776f6eb5
juissi999 <jussi.kaatiala@gmail.com>
2020-02-24 12:52:38 +0200

    Added some packet content extraction

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ fn main() {
         let dn = String::from(&devices[selection as usize].name);
         let dn1: &str = &dn;
 
-        println!("Choosing device {}", dn1);
+        println!("Choosing device {}, printing packets..", dn1);
         let mut cap = pcap::Capture::from_device(dn1).unwrap().open().unwrap(); //.open().unwrap();
 
         // print packets continously
@@ -34,8 +34,30 @@ fn main() {
             // get a packet and print its bytes
             let p = cap.next().unwrap();
             println!("packet {}:", index);
-            println!("{:?}\n{:x?}", p.header, p.data);
+            println!("Eth protocol:{}", eth_protocol(p.data));
+            ipv4_fields(&p.data[16..]);
+            println!("data:{:?}", p.data);
             index += 1;
         }
     }
+}
+
+fn eth_protocol(data: &[u8]) -> String {
+    println!("{}", data[14]);
+
+    if data[14]==0x08 && data[15]==0x00 {
+        String::from("ipv4")
+    } else if data[14]==0x08 && data[15]==0x06 {
+        String::from("ARP")
+    } else if data[14]==0x86 && data[15]==0xdd {
+        String::from("ipv6")
+    } else {
+        String::from("unknown")
+    }
+}
+
+fn ipv4_fields(data: &[u8]) {
+    println!("IP Protocol: {}", &data[9]);
+    println!("source: {:?}", &data[12..16]);
+    println!("destination: {:?}", &data[16..20]);
 }
